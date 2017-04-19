32d910b20b8126f72167ae1d602f2eaa5044f5d8
admarkov <admarkov@lenta.ru>
2017-04-19 20:52:17 +0300

    Abandoned of default Expression class and started to implement abstract Tree class

--- src.cpp
+++ src.cpp
@@ -1,85 +1,9 @@
 #include <iostream>
+#include <string>
 
 #define INPUT_FILE "sample1"
 
 using namespace std;
-
-class Expression {
-public:
-    virtual void print() = 0;
-    virtual Expression* clone() = 0;
-    virtual ~Expression() {
-
-    }
-};
-
-class Const : public Expression {
-    double c;
-public:
-    Const() : c(0) {}
-    Const(double x) : c(x) {}
-    void print()
-    {
-        cout << c;
-    }
-    Expression* clone()
-    {
-        return new Const(c);
-    }
-};
-
-class Var : public Expression {
-public:
-    void print()
-    {
-        cout << 'x';
-    }
-    Expression* clone()
-    {
-        return new Var();
-    }
-};
-
-class Sum : public Expression {
-    Expression *a, *b;
-public:
-    Sum(Expression* l, Expression* r) : a(l), b(r) {}
-    void print()
-    {
-        a->print();
-        cout << '+';
-        b->print();
-    }
-    Expression* clone()
-    {
-        return new Sum(a, b);
-    }
-    ~Sum() {
-        delete a;
-        delete b;
-    }
-};
-
-class Dif : public Expression {
-    Expression *a, *b;
-public:
-    Dif(Expression* l, Expression* r): a(l), b(r) {}
-    void print()
-    {
-        a->print();
-        
-        b->print();
-    }
-};
-
-Expression* operator+(Expression f, Expression g)
-{
-    return new Sum(f, g);
-}
-Expression* operator-(Expression f, Expression g)
-{
-    return new Dif(f, g);
-}
 
 int main ()
 {
---
+++ tree.h
@@ -0,0 +1,15 @@
+//
+// Created by admarkov on 19.04.17.
+//
+
+#ifndef FUNC_COMPOSER_TREE_H
+#define FUNC_COMPOSER_TREE_H
+
+class Node {
+
+};
+
+class Tree {
+
+};
+#endif //FUNC_COMPOSER_TREE_H
