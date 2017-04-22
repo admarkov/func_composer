1d8c6230e6a74cd44324e9e5346fb3b0bed2c00f
admarkov <admarkov@lenta.ru>
2017-04-22 21:09:41 +0300

    Implemented frame library (contains common special types, procedures etc.)

---
+++ frame.h
@@ -0,0 +1,16 @@
+//
+// Created by admarkov on 22.04.17.
+//
+
+#ifndef FUNC_COMPOSER_FRAME_H
+#define FUNC_COMPOSER_FRAME_H
+
+#include <string>
+typedef unsigned int index;
+
+void crash(char info[]="") {
+    puts("Fatal error.");
+    exit(0);
+}
+
+#endif //FUNC_COMPOSER_FRAME_H
