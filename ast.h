40dd0e1348b2c648c21063655b8c68c875a9581f
admarkov <admarkov@lenta.ru>
2017-04-26 16:24:23 +0300

    Abandoned of "var" subclass

--- ast.h
+++ ast.h
@@ -10,29 +10,50 @@
 
 struct Node {
     Node *parent, *left, *right;
+    Node *root;
+    string type;
     std::string data;
-    Node(Node* p = nullptr, std::string d="", Node* l = nullptr, Node* r = nullptr) : parent(p), data(d), left(l), right(r) {}
-    virtual Node* clone(Node* p = nullptr) {
-        Node *n = new Node (p, data, nullptr, nullptr);
-        n->left = left->clone(n);
-        n->right = right->clone(n);
+    Node(Node* p = nullptr, Node* rt=nullptr, string t="", std::string d="", Node* l = nullptr, Node* r = nullptr) : parent(p), type(t), root(rt), data(d), left(l), right(r) {}
+    Node* clone(Node* r, Node* p = nullptr) {
+        Node *n = new Node (p, r, type, data, nullptr, nullptr);
+        if (left!=nullptr) n->left = left->clone(n);
+        if (right!=nullptr) n->right = right->clone(n);
         return n;
     }
-    virtual void del () {
+    void del () {
         if (left!=nullptr) left->del();
         if (right!=nullptr) right->del();
         delete left;
         delete right;
     }
+    static void print(Node *r, int offset = 0)
+    {
+        if(r==nullptr) return;
+        print(r->right, offset+3);
+        for(int i = 0; i<offset; i++)
+            cout<<' ';
+        cout<<r->data<<endl;
+        print(r->left, offset+3);
+    }
+
+    Node* setvar(Node* root, Node *val, string varname, Node* p) {
+        if (type=="variable") {
+            if (data==varname && root==root) {
+                Node* n = clone(val);
+                n->parent = p;
+                return n;
+            }
+        }
+        else {
+            if (left!=nullptr) left->setvar(root, val, varname, this);
+            if (right!=nullptr) right->setvar(root, val, varname, this);
+            return this;
+        }
+    }
+
     ~Node() {
         del();
     }
-};
-
-struct var : Node {
-    std::string name;
-    Node *root;
-    var(std::string n, Node* r, Node* p = nullptr) : name(n), root(r), Node(p) {}
 };
 
 #endif //FUNC_COMPOSER_FTREE_H
