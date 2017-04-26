255a022e1de33babfd3aad801ed890c60f8526c9
admarkov <admarkov@lenta.ru>
2017-04-26 16:42:26 +0300

    Implemented simple sample of tree combinations

--- src.cpp
+++ src.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <string>
-
-#define INPUT_FILE "sample1"
+#include "tokens.h"
+#include "ast.h"
+#include "tree_builder.h"
 
 using namespace std;
 
 int main ()
 {
-    freopen(INPUT_FILE, "r", stdin);
+    TokenStream f("x^2^(3+5)+7");
+    TokenStream x("2*sin(t)");
+    TokenStream c("8");
+    Node* f_ast = buildAST(f);
+    Node* x_ast = buildAST(x);
+    Node* c_ast = buildAST(c);
+    x_ast->setvar(x_ast, f_ast, "t");
+    Node::print(x_ast);
 }
