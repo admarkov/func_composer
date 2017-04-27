343cac3c240ea9b93d9ed761222dfba6f7f24de5
admarkov <admarkov@lenta.ru>
2017-04-27 22:19:50 +0300

    Implemented "setvar" and "print" functions

--- ftree.h
+++ ftree.h
@@ -2,6 +2,7 @@
 // Created by admarkov on 27.04.17.
 //
 
+#include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
@@ -17,11 +18,29 @@ public:
     vector<string> varlist;
     vector<fNode*> vallist;
     fNode() {}
-    fNode* clone(fNode* f) {
+    fNode(fNode* f) {
         type=f->type;
         name=f->name;
         for (string v: f->varlist) {
             push_var(v);
+        }
+    }
+    void setvar(string var, fNode* f) {
+        int i=0;
+        while (varlist[i]!=var) i++;
+        vallist[i]=new fNode(f);
+    }
+    void print() {
+        if (type=="function") {
+            cout<<name<<"(";
+            for (int i=0; i<varlist.size(); i++) {
+                if (vallist[i]!=nullptr) vallist[i]->print();
+                if (i!=vallist.size()-1) cout<<",";
+            }
+            cout<<")";
+        }
+        else {
+            cout<<name;
         }
     }
 };
